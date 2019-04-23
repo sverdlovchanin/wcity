655fcb3695945651da2500d82d6cf935c8436532
bela-182@mail.ru <bela-182@mail.ru>
2019-04-23 21:06:43 +0300

    1.0

--- wcity/urls.py
+++ wcity/urls.py
@@ -14,12 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+	path('accounts/logout/', LogoutView.as_view(
+	template_name='fenster/loginpage.html'),
+		name='logout'),
     path('fenster/', include('fenster.urls')),
     path('freeaddr/', include('freeaddr.urls')),
 	path('accounts/login/', LoginView.as_view(
